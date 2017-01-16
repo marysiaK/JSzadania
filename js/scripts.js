17338fefe241569381b39f9577b16aa58d8aa6dc
marysiaK <marysia.kolaczynska@gmail.com>
2017-01-16 20:34:00 +0100

    zamiana 1 wyrazu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-var text = 'Papugi kosz¹ trawê, unosz¹c siê nad ziemi¹ 15 centymetrów, na czerwonych dywanach.';
-var animal= 'Zielone s³onie';
-	animal = animal.toUpperCase();
 
-	text = text.replace('Papugi',animal);
+var text = 'Papugi kosza trawe, unoszac sie nad ziemia 15 centymetrow, na czerwonych dywanach.';
+var animal= 'Zielone slonie';
 
+var animal= text.toUpperCase();
+var textReplace = text.replace('Papugi',animal);
 console.log(text.substr(0,text.length/2));
+
+var animal2= 'Czerwone slonie';
+var animal2 = animal2.toUpperCase();
+
+var firstWord = text.split(' ',1); 
+var text3 = text.replace(firstWord, animal2);
+console.log(text.substr(0,text3.length/2));
+console.log(text3);
